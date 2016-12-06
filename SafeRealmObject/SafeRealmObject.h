7ba0248955d46077137fe8c03dd9717aab558486
Adam Fish <af@realm.io>
2016-12-06 00:52:53 -0800

    Added xcode project for carthage support

---
+++ SafeRealmObject/SafeRealmObject.h
@@ -0,0 +1,17 @@
+//
+//  SafeRealmObject.h
+//  SafeRealmObject
+//
+//  Created by Adam Fish on 12/6/16.
+//  Copyright © 2016 Adam Fish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SafeRealmObject.
+FOUNDATION_EXPORT double SafeRealmObjectVersionNumber;
+
+//! Project version string for SafeRealmObject.
+FOUNDATION_EXPORT const unsigned char SafeRealmObjectVersionString[];
+
+
