ae55d17725d9b71a6267c36e1aab8b10911af018
Adam Fish <af@realm.io>
2016-12-06 01:58:58 -0800

    Additional fixes for Carthage support

---
+++ RBQSafeRealmObject/RBQSRO.h
@@ -0,0 +1,18 @@
+//
+//  RBQSRO.h
+//  RBQSafeRealmObject
+//
+//  Created by Adam Fish on 12/6/16.
+//  Copyright © 2016 Adam Fish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SafeRealmObject.
+FOUNDATION_EXPORT double RBQSafeRealmObjectVersionNumber;
+
+//! Project version string for SafeRealmObject.
+FOUNDATION_EXPORT const unsigned char RBQSafeRealmObjectVersionString[];
+
+#import <RBQSafeRealmObject/RBQSafeRealmObject.h>
+#import <RBQSafeRealmObject/RLMObject+SafeObject.h>
